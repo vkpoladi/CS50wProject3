43bf2608b5290988bb5998208d2a2933be2efee8
vkpoladi <vkpoladi@seas.upenn.edu>
2021-05-19 13:35:32 -0500

    Displaying mail items

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -20,6 +20,33 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').onsubmit = function(event) {
+    event.preventDefault();
+    fetch('/emails', {
+    method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => {
+    return response.json()
+    })
+    .then(result => {
+    console.log(result);
+    load_mailbox('sent')
+    })
+    //Incase API is down or has issues
+    .catch(error => {
+        console.log('Error:', error);
+    });
+    //Not needed, but good practice
+    return false;
+    
+  }
+  
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +57,30 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show list of emails for the specific mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    // Declaring int (type) in for loop head causes error; not sure why
+    for (i = 0; i < emails.length; i++) {
+      element = document.createElement('div');
+      element.className = 'email-item';
+      element.style.border = "thick solid #0000FF";
+      if (emails[i].read) {
+        element.style.backgroundColor = "gray";
+      } else {
+        element.style.backgroundColor = "white";
+      }
+      element.innerHTML = emails[i].sender + "\t" + emails[i].subject + "\t" + emails[i].timestamp;
+      document.querySelector('#emails-view').append(element);
+    }
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
+  return false;
+
+
 }
